af566c7004e18e063a8c0a0e0d6eae4aad69d7c1
Santiago Santiago Gonzalez <sasa@ciklum.com>
2019-10-01 15:09:46 +0200

    CU-4 Persisting rounds after user reset

--- src/main/java/com/santi/rockpaperscissors/storage/InMemoryMapStorage.java
+++ src/main/java/com/santi/rockpaperscissors/storage/InMemoryMapStorage.java
@@ -15,30 +15,38 @@ public class InMemoryMapStorage implements RockPaperScissorsStorage {
 
     private static final Long INITIAL_ID = Long.valueOf(1l);
 
-    private Map<Long, List<RoundResult>> userMap;
+    private Map<Long, List<RoundResult>> userRounds;
+    private List<RoundResult> rounds;
 
     public InMemoryMapStorage() {
-        this.userMap = new ConcurrentHashMap<>();
+        this.userRounds = new ConcurrentHashMap<>();
+        this.rounds = Collections.synchronizedList(new ArrayList<>());
     }
 
     public InMemoryMapStorage(Map<Long, List<RoundResult>> map) {
         this();
-        this.userMap.putAll(map);
+        this.userRounds.putAll(map);
+        this.rounds.addAll(
+            map
+                .values()
+                .stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList()));
     }
 
     @Override
     public Long createUser() {
-        return userMap
+        return userRounds
             .keySet()
             .stream()
             .max(Comparator.naturalOrder())
             .map(max -> {
                 long newId = max + 1;
-                userMap.put(newId, new ArrayList<>());
+                userRounds.put(newId, new ArrayList<>());
                 return newId;
             })
             .orElseGet(() -> {
-                userMap.put(INITIAL_ID, new ArrayList<>());
+                userRounds.put(INITIAL_ID, new ArrayList<>());
                 return INITIAL_ID;
             });
     }
@@ -47,7 +55,7 @@ public class InMemoryMapStorage implements RockPaperScissorsStorage {
     public List<RoundResult> getRoundsByUserId(Long userId) {
         Assert.notNull(userId, "userId cannot be null");
         return Optional
-            .ofNullable(userMap.get(userId))
+            .ofNullable(userRounds.get(userId))
             .orElse(Collections.emptyList());
     }
 
@@ -55,22 +63,19 @@ public class InMemoryMapStorage implements RockPaperScissorsStorage {
     public void saveRound(Long userId, RoundResult roundResult) {
         Assert.notNull(userId, "userId cannot be null");
         Assert.notNull(roundResult, "roundResult cannot be null");
-        userMap.putIfAbsent(userId, new ArrayList<>());
-        userMap.get(userId).add(roundResult);
+        userRounds.putIfAbsent(userId, new ArrayList<>());
+        userRounds.get(userId).add(roundResult);
+        rounds.add(roundResult);
     }
 
     @Override
     public List<RoundResult> getRounds() {
-        return userMap
-            .values()
-            .stream()
-            .flatMap(List::stream)
-            .collect(Collectors.toList());
+        return this.rounds;
     }
 
     @Override
     public void removeRoundsByUserId(Long userId) {
-        userMap.computeIfPresent(
+        userRounds.computeIfPresent(
             userId,
             (key, list) -> {
                 list.clear();
